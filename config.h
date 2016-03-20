6152bc1ef58c583623af76d6c5ff40ad43dc0691
upgradeadvice <upgradeadvice@gmail.com>
2016-03-20 02:27:20 -0500

    config.h fix

--- config.h
+++ config.h
@@ -6,9 +6,20 @@
 #define HAVE_STRING_H 1
 #define HAVE_SYS_STAT_H 1
 #define HAVE_SYS_TYPES_H 1
-#define MAJOR_VERSION 3
-#define MICRO_VERSION 9
-#define MINOR_VERSION 0
 #define PACKAGE "libqrencode"
-#define VERSION "3.9.0"
 #define inline _inline
+#define MAJOR_VERSION 3
+#define MINOR_VERSION 4
+#define MICRO_VERSION 4
+#define VERSION "3.4.4"
+
+#define __STATIC static
+
+#if defined(_WIN32) || defined(_WIN64)
+  #define strcasecmp _stricmp
+  #define strncasecmp _strnicmp
+  #if _MSC_VER < 1900
+    #define snprintf _snprintf
+    #define vsnprintf _vsnprintf
+  #endif
+#endif
