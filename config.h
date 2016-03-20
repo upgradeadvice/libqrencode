c4ff0530451682037c930ca99451c39ae166dc63
upgradeadvice <upgradeadvice@gmail.com>
2016-03-20 01:53:13 -0500

    msvc 2015

---
+++ config.h
@@ -0,0 +1,14 @@
+#define HAVE_MEMORY_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRDUP 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define MAJOR_VERSION 3
+#define MICRO_VERSION 9
+#define MINOR_VERSION 0
+#define PACKAGE "libqrencode"
+#define VERSION "3.9.0"
+#define inline _inline
